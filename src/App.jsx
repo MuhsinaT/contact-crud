8d76ecd456dfa86e144e77e5cd8654670d29b3cb
Muhsina parveen <muhsinamuhsina703@gmail.com>
2024-09-12 20:31:54 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from './Components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home';
 import Contact from './Components/Contact';
-
 import { Route,Routes } from 'react-router-dom';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
