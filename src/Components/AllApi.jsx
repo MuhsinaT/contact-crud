c6ee1535fb4675aa79940b71fe8be3559ab53c53
Muhsina parveen <muhsinamuhsina703@gmail.com>
2024-09-12 20:54:23 +0530

    first

--- src/Components/AllApi.jsx
+++ src/Components/AllApi.jsx
@@ -2,18 +2,18 @@ import axios from "axios";
 const base_url="https://mdb-server-qmhl.onrender.com"
 
 export const addContact =async(data) =>{
-    return await axios.post(`${base_url}contacts`, data)
+    return await axios.post(`${base_url}/contacts`, data)
   }
 
 export const getContact=async()=>{
-  return await axios.get(`${base_url}contacts`)
+  return await axios.get(`${base_url}/contacts`)
 }
 
 export const deleteContact=async(id)=>{
-  return await axios.delete(`${base_url}contacts/${id}`)
+  return await axios.delete(`${base_url}/contacts/${id}`)
 }
 
 
 export const updateContact = async (id, data) => {
-  return await axios.put(`${base_url}contacts/${id}`, data);
+  return await axios.put(`${base_url}/contacts/${id}`, data);
 };
